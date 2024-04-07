e7ba6fa1238dfe71b2b8ce3d041e4f79a01e2686
Luis Ceren <luiscerenc18@gmail.com>
2024-04-06 23:44:30 -0500

    Id changed into int

--- src/domain/validators/invoices/validators.ts
+++ src/domain/validators/invoices/validators.ts
@@ -3,15 +3,14 @@ import { body } from "express-validator";
 export const createInvoiceValidator = [
   body("clientId", "clientId property is required and cannot be empty")
     .exists()
-    .notEmpty(),
+    .notEmpty()
+    .isInt(),
   body("invoiceImage", "InvoiceImage property cannot be empty")
     .optional()
     .notEmpty(),
   body("invoiceProducts", "Invoice Product is required and cannot be empty")
     .notEmpty()
     .isArray(),
-  body("invoiceProducts.*.product", "Product must be a string").isString(),
+  body("invoiceProducts.*.product", "Product must be a string").isInt(),
   body("invoiceProducts.*.quantity", "Quantity must be a number").isNumeric(),
 ];
-
-
